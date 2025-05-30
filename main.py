a576909a55131e3540ab829b782a70446fe76617
pzoli <papp.zoltan@integrity.hu>
2025-05-30 11:53:49 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+
+from dotenv import load_dotenv
+from kafka import KafkaConsumer, KafkaProducer
+load_dotenv()
+
+producer = KafkaProducer(bootstrap_servers=[os.getenv("KAFKA_URL")],
+                        security_protocol = 'SASL_PLAINTEXT',
+                        sasl_mechanism = 'PLAIN',
+                        sasl_plain_username = os.getenv("KAFKA_USER"),
+                        sasl_plain_password = os.getenv("KAFKA_PASSWORD")
+                         )
+print(f"producer connected: {producer.bootstrap_connected()}")
+for i in range(5):
+    producer.send(os.getenv("KAFKA_TOPIC"), ('msg' + str(i)).encode() )
+producer.flush()
+producer.close()
+print("Messages are sent")
+
+print("start consumer")
+consumer = KafkaConsumer(os.getenv("KAFKA_TOPIC"),
+                         #group_id='group1',
+                         group_id=None,
+                         bootstrap_servers=[os.getenv("KAFKA_URL")],
+                         security_protocol='SASL_PLAINTEXT',
+                         sasl_mechanism='PLAIN',
+                         sasl_plain_username=os.getenv("KAFKA_USER"),
+                         sasl_plain_password=os.getenv("KAFKA_PASSWORD"),
+                          auto_offset_reset=os.getenv("KAFKA_OFFSET")
+                         )
+print(f"consumer connected: {consumer.bootstrap_connected()}")
+
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
+
